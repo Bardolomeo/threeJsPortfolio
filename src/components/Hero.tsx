9a5aa3c00547bc3e5e009964d5bb4ac65b1e3abc
Bardolomeo <sapiobard@gmail.com>
2024-09-23 19:40:37 +0200

    prove reflection

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
-import React, { useEffect, useRef, useState } from 'react'
+import { Environment, Image, MeshReflectorMaterial, Texture } from '@react-three/drei';
+import { Canvas, ThreeElements, useFrame, useLoader } from '@react-three/fiber';
+import React, { useRef } from 'react'
 import * as THREE from 'three';
+
+let startingX = -10
 
 const Hero = () => {
 
 	function Box(props : ThreeElements['mesh'])
 	{
 		const ref = useRef<THREE.Mesh>(null!);
-		const randomDelta = 0.01 * ((Math.random() * 2) + 1);
+		const randomDelta = 0.01 * ((Math.random() * 1.5) + 1);
+		const randomStartingX = startingX + (Math.random() * 4);
+		startingX += 5;
 
-			useFrame((visible, delta ) => {
+		useFrame(() => {
 				if (ref.current.position.y < 9.0)
 				{
 					console.log(ref.current.position.y);
@@ -18,27 +23,39 @@ const Hero = () => {
 				}
 				else
 				{
-					ref.current.position.x = props.xStarting - ((Math.random() * 2));
+					ref.current.position.x = randomStartingX - ((Math.random() * 2));
 					ref.current.position.y = -10;
 					ref.current.scale.setScalar(Math.random() + 0.5);
 				}
 		}, 0)
+		
 		return (
 			<mesh {...props}
 				ref={ref}
 				scale={Math.random() + 0.5}
-				position={[ props.xStarting, 10 - (Math.random() * 20), 1]}
+				position={[ randomStartingX, 10 - (Math.random() * 20), 0]}
 
 			>
 				<sphereGeometry />
-				<meshPhysicalMaterial color='#72B2F2'
-				emissive={'#72B2F2'}
+				<MeshReflectorMaterial
+    				blur={[0, 0]}
+    				resolution={520}
+    				mixBlur={1}
+    				mixStrength={50}
+    				roughness={1}
+    				depthScale={1.2}
+    				minDepthThreshold={0.4}
+    				maxDepthThreshold={1.4}
+    				metalness={0}
+					mirror={0}
+        		/>
+				{/* <meshPhysicalMaterial color='#72B2F2'
+				emissive={'#000000'}
 				clearcoat={1}
 				clearcoatRoughness={0}
 				transparent={true}
-				opacity={0.5}
-				iridescence={1}
-				specularColor={0.5}/>
+				opacity={1}
+				/> */}
 			</mesh>
 		)
 	}
@@ -57,26 +74,35 @@ const Hero = () => {
 		camera.position.y = 0;
 	}
 
-	//Mesh
-	const geometry = new THREE.BoxGeometry();
-	const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
-	const cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-
 	//light
+	const boxRef = new Array(5);
+
+	boxRef[0] = useRef<THREE.Mesh>(null!);
+	boxRef[1] = useRef<THREE.Mesh>(null!);
+	boxRef[2] = useRef<THREE.Mesh>(null!);
+	boxRef[3] = useRef<THREE.Mesh>(null!);
+
 	const light = new THREE.DirectionalLight(0x9cdba6, 10);
 	light.position.set(1, 1, 1);
-	scene.add(light);
+	const dirLight = useRef<THREE.DirectionalLight>(null);
+
+	const colorMap = useLoader(THREE.TextureLoader, 'ShadowTheHedgehogSA2.png')
 
 	return (
 		<div className='w-[100vw] h-[100vh]'>
-			<Canvas camera={camera} className='w-full h-full'>
-			<directionalLight xStarting={0} color={'#ffffff'} position={[0 ,0, 20]} intensity={1}/>
-				<Box xStarting={-10 + Math.random() * 4}/>
-				<Box xStarting={-5 + Math.random() * 4}/>
-				<Box xStarting={0 + Math.random() * 4}/>
-				<Box xStarting={5 + Math.random() * 4}/>
-				<Box xStarting={10 + Math.random() * 4}/>
+			<Canvas camera={camera} className='w-full h-full' >
+				<directionalLight color={0xffffff} intensity={5} position={[0,0,20]}/>
+				<fog attach="fog" args={['#191920', 0, 15]} />
+				<color attach="background" args={['#191920']} />
+				<Box ref={boxRef[0]} />
+				<Box ref={boxRef[1]}/>
+				<Box ref={boxRef[2]}/>
+				<Box ref={boxRef[3]}/>
+				<Box />
+				<mesh position={[0, 0, 0]} rotation={[0,10,0,'XYZ']} scale={5.0}>
+					<boxGeometry />
+					<meshStandardMaterial map={colorMap}/>
+				</mesh>
 			</Canvas>
 		</div>
   )
